29157e192647accde2674745675d3033ca84709d
Pie is Spy <95068281+PieIsSpy@users.noreply.github.com>
2022-04-29 14:14:01 +0800

    Create apple.py

---
+++ apple.py
@@ -0,0 +1,13 @@
+a = 7
+b = 4
+print("Hello world")
+print("A man shared 4 apples with a man with 7 apples. How many apples are there now?")
+c = a + b
+print(c)
+print("You ate one apple. How many are there now?")
+d = c-1
+print (d)
+print("Yum")
+if d == 10:
+    print("You found it tasty. You ate the rest and left nothing behind. Silly man.")
+print("Wait. Did you just do the bite of 87?")
